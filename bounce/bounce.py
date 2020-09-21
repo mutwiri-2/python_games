37d0407cc29f48027604ce6d23dbfc34d94078cb
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-21 22:42:20 +0300

    make the ball bounce

--- bounce/bounce.py
+++ bounce/bounce.py
@@ -13,11 +13,19 @@ tk.update()
 class Ball():
     def __init__(self, canvas, color):
         self.canvas = canvas
+        self.x = 0
+        self.y = -1
+        self.canvas_height = self.canvas.winfo_height()
         self.id = canvas.create_oval(10,10,30,30, fill=color)
         self.canvas.move(self.id, 245, 100)
     
     def draw(self):
-        self.canvas.move(self.id, 0, -1)
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.canvas_height:
+            self.y = -1
 
 ball = Ball(canvas, "blue")
 
