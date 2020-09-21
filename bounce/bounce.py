f75b32cae33b51b20785d40e72e69accb383e44f
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-21 22:12:31 +0300

    add ball class

--- bounce/bounce.py
+++ bounce/bounce.py
@@ -8,3 +8,13 @@ tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
+
+# ball class
+class Ball():
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10,10,30,30, fill=color)
+        self.canvas.move(self.id, 245, 100)
+    
+    def draw(self):
+        pass
