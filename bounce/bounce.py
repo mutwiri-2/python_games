a315b05dd686f557af40b252819f091d57414892
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-22 22:25:06 +0300

    change canvas height

--- bounce/bounce.py
+++ bounce/bounce.py
@@ -23,7 +23,7 @@ tk = Tk()
 tk.title("Bounce")
 tk.resizable(0,0)
 tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
