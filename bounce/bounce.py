038070c8055585ecd6f10d543b68f75aa6e1472f
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-23 00:20:18 +0300

    make ball bounce around screen horizontally and vertically

--- bounce/bounce.py
+++ bounce/bounce.py
@@ -7,17 +7,23 @@ class Ball():
         self.canvas = canvas
         self.id = canvas.create_oval(10,10,30,30, fill=color)
         self.canvas.move(self.id, 245, 100)
-        self.x = 0
-        self.y = -1
+        starts = random.randrange(-3,4)
+        self.x = starts
+        self.y = -3
         self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
     
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         if pos[1] <= 0:
-            self.y = 1
+            self.y = 3
         if pos[3] >= self.canvas_height:
-            self.y = -1
+            self.y = -3
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
 
 tk = Tk()
 tk.title("Bounce")
