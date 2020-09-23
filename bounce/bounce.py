831c86c6c84a78f32e737720d34ad0f875dbc741
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-23 12:32:02 +0300

    bind left and arrow key to turn left and turn right functions

--- bounce/bounce.py
+++ bounce/bounce.py
@@ -30,9 +30,20 @@ class Paddle:
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
         self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
 
     def draw(self):
         pass
+
+    def turn_left(self, evt):
+        self.x = -2
+    
+    def turn_right(self, evt):
+        self.x = 2
+
 
 
 
