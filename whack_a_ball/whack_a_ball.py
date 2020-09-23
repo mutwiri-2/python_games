403213c0c11a7fb6786738aa186885d16137d1f1
mutwiri-2 <devmutwiri2@gmail.com>
2020-09-23 19:00:49 +0300

    whack-a-ball

--- whack_a_ball/whack_a_ball.py
+++ whack_a_ball/whack_a_ball.py
@@ -3,8 +3,9 @@ import time, random
 
 # ball class
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10,10,30,30, fill=color)
         self.canvas.move(self.id, 245, 100)
         starts = random.randrange(-3,4)
@@ -12,6 +13,14 @@ class Ball:
         self.y = -3
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+    
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
     
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -19,11 +28,40 @@ class Ball:
         if pos[1] <= 0:
             self.y = 3
         if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if self.hit_paddle(pos):
             self.y = -3
         if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
+
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+    def turn_left(self, evt):
+        self.x = -2
+    
+    def turn_right(self, evt):
+        self.x = 2
+
+
+
 
 tk = Tk()
 tk.title("Bounce")
@@ -33,11 +71,14 @@ canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
-ball = Ball(canvas, "blue")
+paddle = Paddle(canvas, 'red')
+ball = Ball(canvas, paddle, "blue")
 
 # main program loop
 while True:
-    ball.draw()
+    if not ball.hit_bottom:
+        ball.draw()
+        paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
